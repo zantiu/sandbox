d525551d04e48d276be1a11b43a4adc4f5d6da7e
Viresh Navalli <viresh-r.navalli@capgemini.com>
2025-09-10 05:19:30 +0000

    add cleanup modified script

--- pipeline/harbor/cleanup_harbor.sh
+++ pipeline/harbor/cleanup_harbor.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 set -e
 
-echo "Stopping all running containers..."
-docker ps -q | xargs -r docker stop
+echo "Stopping Harbor containers..."
+# Stop only Harbor-related containers
+docker ps --filter "name=harbor" --format "{{.ID}}" | xargs -r docker stop
+docker ps --filter "ancestor=goharbor/*" --format "{{.ID}}" | xargs -r docker stop
 
-echo "Removing all containers (including Harbor and others)..."
-docker ps -aq | xargs -r docker rm -f
+echo "Removing Harbor containers..."
+# Remove only Harbor-related containers
+docker ps -a --filter "name=harbor" --format "{{.ID}}" | xargs -r docker rm -f
+docker ps -a --filter "ancestor=goharbor/*" --format "{{.ID}}" | xargs -r docker rm -f
 
 echo "Removing Harbor Docker images..."
 # Force remove Harbor-related images
 docker images --filter=reference='goharbor/*' --format "{{.ID}}" | xargs -r docker rmi -f
+docker images --filter=reference='*harbor*' --format "{{.ID}}" | xargs -r docker rmi -f
 
-echo "Removing all Docker volumes..."
-docker volume ls --format "{{.Name}}" | xargs -r docker volume rm -f
+echo "Removing Harbor Docker volumes..."
+# Remove only Harbor-related volumes
+docker volume ls --filter "name=harbor" --format "{{.Name}}" | xargs -r docker volume rm -f
 
-echo "Removing all Docker networks..."
-docker network ls --format "{{.ID}}" | xargs -r docker network rm -f || echo "Some networks could not be removed, possibly in use."
+echo "Removing Harbor Docker networks..."
+# Remove only Harbor-related networks
+docker network ls --filter "name=harbor" --format "{{.ID}}" | xargs -r docker network rm
 
 echo "Removing Harbor installer files..."
 rm -rf harbor harbor-offline-installer-*
 
-echo "Cleanup complete!"
+echo "Harbor cleanup complete!"
 
